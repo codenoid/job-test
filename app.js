3bf96ddc1d7a02a52e921d1551aa1d5303dce5e4
Rubi Jihantoro <jihantoro8@gmail.com>
2018-02-21 04:59:15 +0700

    beta

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ const ejs = require('ejs')
 const functions = require('./script/functions')
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  functions.calculate_ongkir(7, 17, 237240)
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+app.get('/', function (req, res) { res.render('index') })
+
+app.get('/calculate', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	var la1   = req.query.lat1
+	,	lo1   = req.query.lon1
+	,	la2   = req.query.lat2
+	,	lo2   = req.query.lon2
+	,	date  = req.query.h
+	,	price = req.query.price
+	var km   = functions.harvesine({lat1: la1, lon1: lo1, lat2: la2, lon2: lo2})
+	var kms  = '' + km;
+	var rkm  = kms[0] + "." + kms[2]
+	var fare = functions.fare(km, date, price);
+	var ret  = {"distance": rkm, "cost": fare.cost, "share": fare.share}
+    res.send(JSON.stringify(ret));
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Node App Running on port 3000!'))
--- script/functions.js
+++ script/functions.js
@@ -1,34 +1,68 @@
 module.exports = {
 
-  calculate_ongkir: function (km, time, price) {
-    var cost = km <= 1 ? 4000 : 2000
-    , 	cost = cost * km
-    ,	rush = time >= 16 && time <= 19 ? 3000 : 0
-    ,	cost = cost + rush
-    ,	shop = this.shopCost(price)
-    ,	cost = cost + shop
-    ,	share= this.tenPercent(cost)
-    console.log(share)
-  },
+	// <= 1 KM += 4000 # price
+	//  > 1 KM += 2000 # price
+	// 16 - 19 += 3000 # rush hour
+	// 100k kelipatan 1 dari 50k += 2000 # kelipatan harga 50ribu
+	// 26000/10% = 2600 # share
+    fare: function(km, time, price) {
+        var km = parseInt(km),
+            km = km < 1 ? 1 : km,
+            cost = km <= 1 ? 4000 : 2000,
+            cost = cost * km,
+            rush = time >= 16 && time <= 19 ? 3000 : 0,
+            cost = cost + rush,
+            shop = this.shopCost(price),
+            cost = cost + shop,
+            share = this.tenPercent(cost)
+        var result = {
+            "cost": cost,
+            "share": share
+        }
+        return result
+    },
 
-  isInt : function (value) {
-	var x
-	if (isNaN(value)) return false
-	x = parseFloat(value)
-	return (x | 0) === x
-  },
+    isInt: function(value) {
+        var x
+        if (isNaN(value)) return false
+        x = parseFloat(value)
+        return (x | 0) === x
+    },
 
-  shopCost : function (price) {
-  	if (price <= 50001 || price <= 99999) return 0
-  	var total = 0
-	for (var index = 0; index < price; index++) {
-	  if (this.isInt(index / 50000) && index != 0) total += 1
-	}
-	return 2000 * total
-  },
+    shopCost: function(price) {
+        if (price <= 50001 || price <= 99999) return 0
+        var total = 0
+        for (var index = 0; index < price; index++) {
+            if (this.isInt(index / 50000) && index != 0) total += 1
+        }
+        return 2000 * total
+    },
 
-  tenPercent : function (cost) {
-  	return (10 / 100) * cost;
-  }
+    tenPercent: function(cost) {
+        return (10 / 100) * cost;
+    },
+
+    // Harvesine Logic - Earth Radius = 6371 (KM)
+    // https://stackoverflow.com/a/27943/9137919
+    harvesine: function(latlon) {
+        var lat1 = latlon.lat1,
+            lon1 = latlon.lon1,
+            lat2 = latlon.lat2,
+            lon2 = latlon.lon2
+        var earthr = 6371
+        var dLat = this.deg2rad(lat2 - lat1)
+        var dLon = this.deg2rad(lon2 - lon1)
+        var a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2)
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+        var km = earthr * c
+        return km
+    },
+
+    deg2rad: function(deg) {
+        return deg * (Math.PI / 180)
+    }
 
 };
